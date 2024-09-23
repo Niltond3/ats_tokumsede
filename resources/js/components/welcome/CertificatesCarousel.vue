c2bdecda067660a6bf253b17718eaa61f3ae0b8a
niltond3 <niltond83@gmail.com>
2024-09-23 09:30:57 -0300

    style: support page carousel style

--- resources/js/components/welcome/CertificatesCarousel.vue
+++ resources/js/components/welcome/CertificatesCarousel.vue
@@ -3,79 +3,99 @@ import { ref } from 'vue'
 import { watchOnce } from '@vueuse/core'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import { Card, CardContent } from '@/components/ui/card'
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
+import { Label } from '@/components/ui/label'
+
 
 const emblaMainApi = ref()
 const emblaThumbnailApi = ref()
 const selectedIndex = ref(0)
 
 const certificates = [
-    {index:0, url: "/images/certificates/aesa.png"},
-    {index:1, url: "/images/certificates/agevisa.png"},
-    {index:2, url: "/images/certificates/Alvara-funcionamento-municipal.png"},
-    {index:3, url: "/images/certificates/conselho-de-quimica.png"},
-    {index:4, url: "/images/certificates/Sudema.png"}
+    { index: 0, url: "/images/certificates/aesa.png", title: "AESA" },
+    { index: 1, url: "/images/certificates/agevisa.png", title: "AGEVISA" },
+    { index: 2, url: "/images/certificates/Alvara-funcionamento-municipal.png", title: "Alvara de funcionamento municipal" },
+    { index: 3, url: "/images/certificates/conselho-de-quimica.png", title: "Conselho de Química" },
+    { index: 4, url: "/images/certificates/Sudema.png", title: "Sudema" }
 ]
 
 function onSelect() {
-  if (!emblaMainApi.value || !emblaThumbnailApi.value)
-    return
-  selectedIndex.value = emblaMainApi.value.selectedScrollSnap()
-  emblaThumbnailApi.value.scrollTo(emblaMainApi.value.selectedScrollSnap())
+    if (!emblaMainApi.value || !emblaThumbnailApi.value)
+        return
+    selectedIndex.value = emblaMainApi.value.selectedScrollSnap()
+    emblaThumbnailApi.value.scrollTo(emblaMainApi.value.selectedScrollSnap())
 }
 
 function onThumbClick(index) {
     console.log(emblaMainApi.value)
-  if (!emblaMainApi.value || !emblaThumbnailApi.value)
-    return
-  emblaMainApi.value.scrollTo(index)
+    if (!emblaMainApi.value || !emblaThumbnailApi.value)
+        return
+    emblaMainApi.value.scrollTo(index)
 }
 
 watchOnce(emblaMainApi, (emblaMainApi) => {
-  if (!emblaMainApi)
-    return
+    if (!emblaMainApi)
+        return
 
-  onSelect()
-  emblaMainApi.on('select', onSelect)
-  emblaMainApi.on('reInit', onSelect)
+    onSelect()
+    emblaMainApi.on('select', onSelect)
+    emblaMainApi.on('reInit', onSelect)
 })
 </script>
 
 <template>
-  <div class="w-full sm:w-auto flex items-center flex-col">
-    <Carousel
-      class="relative w-full max-w-xs"
-      @init-api="(val) => emblaMainApi = val"
-    >
-      <CarouselContent>
-        <CarouselItem v-for="{index,url} in certificates" :key="index">
-          <div class="p-1">
-            <Card class="border-[#00639a]">
-              <CardContent class="flex aspect-square items-center justify-center p-6">
-                <img :src="url"></img>
-              </CardContent>
-            </Card>
-          </div>
-        </CarouselItem>
-      </CarouselContent>
-      <CarouselPrevious class="border-sky-300 border-2 text-sky-200 hover:text-sky-600 transition-colors"/>
-      <CarouselNext class="border-sky-300 border-2 text-sky-200 hover:text-sky-600 transition-colors"/>
-    </Carousel>
+    <div class="w-full sm:w-auto flex items-center flex-col">
+        <Carousel class="relative w-full max-w-xs" @init-api="(val) => emblaMainApi = val">
+            <CarouselContent>
+                <CarouselItem v-for="{ index, url, title } in certificates" :key="index">
+                    <div class="p-1">
+                        <Dialog>
+                            <DialogTrigger as-child>
+                                <Card class="border-[#00639a] cursor-pointer">
+                                    <CardContent class="flex aspect-square items-center justify-center p-6">
+                                        <img :src="url"></img>
+                                    </CardContent>
+                                </Card>
+                            </DialogTrigger>
 
-    <Carousel
-      class="relative w-full max-w-xs"
-      @init-api="(val) => emblaThumbnailApi = val"
-    >
-      <CarouselContent class="flex gap-1 ml-0">
-        <CarouselItem v-for="{index, url} in certificates" :key="index" class="pl-0 basis-1/4 cursor-pointer" @click="onThumbClick(index)">
-          <div class="p-1" :class="index === selectedIndex ? '' : 'opacity-50'">
-            <Card>
-              <CardContent class="flex aspect-square items-center justify-center p-6">
-                <img :src="url"></img>
-              </CardContent>
-            </Card>
-          </div>
-        </CarouselItem>
-      </CarouselContent>
-    </Carousel>
-  </div>
+                            <DialogContent class="max-w-full sm:max-w-md">
+                                <DialogHeader>
+                                    <DialogTitle>{{ title }}</DialogTitle>
+                                </DialogHeader>
+                                <div class="flex items-center space-x-2">
+                                    <img :src="url"></img>
+                                </div>
+                            </DialogContent>
+                        </Dialog>
+                    </div>
+                </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious class="border-sky-300 border-2 text-sky-200 hover:text-sky-600 transition-colors" />
+            <CarouselNext class="border-sky-300 border-2 text-sky-200 hover:text-sky-600 transition-colors" />
+        </Carousel>
+
+        <Carousel class="relative w-full max-w-xs" @init-api="(val) => emblaThumbnailApi = val">
+            <CarouselContent class="flex gap-1 ml-0 [&_*]:border-[#00639a]">
+                <CarouselItem v-for="{ index, url } in certificates" :key="index" class="pl-0 basis-1/4 cursor-pointer "
+                    @click="onThumbClick(index)">
+                    <div class="p-1" :class="index === selectedIndex ? '' : 'opacity-50'">
+                        <Card>
+                            <CardContent class="flex aspect-square items-center justify-center p-6">
+                                <img :src="url"></img>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </CarouselItem>
+            </CarouselContent>
+        </Carousel>
+    </div>
 </template>
--- resources/js/components/welcome/Support.vue
+++ resources/js/components/welcome/Support.vue
@@ -51,7 +51,8 @@ import CertificatesCarousel from './CertificatesCarousel.vue';
                     <div>
                         <h4>Passo 2</h4>
                         <p class="flex items-center flex-wrap">
-                            <span class="flex items-center">Faça seu <a :href="route('register')">Cadastro</a>! É Rápido.
+                            <span class="flex items-center">Faça seu <a :href="route('register')">Cadastro</a>! É
+                                Rápido.
                                 <i class="icon text-yellow-600">
                                     <FlashIcon />
                                 </i>
@@ -95,14 +96,15 @@ import CertificatesCarousel from './CertificatesCarousel.vue';
                 </div>
 
             </div>
-            <div class="gap-4 flex flex-col text-slate-900">
-                <h2 class="my-8 text-slate-700">Licenças e Certificados</h2>
+            <div class="gap-4 flex flex-col [&_*]:text-slate-700">
+                <h2 class="my-8">Licenças e Certificados</h2>
                 <p>
                     Transparecemos as nossas Licenças e Certificações!
                 </p>
                 <p>
                     Em respeito a <a href="https://www.planalto.gov.br/ccivil_03/_ato2015-2018/2017/lei/l13425.htm"
-                        target="_blank" class="underline">Lei 13.425 de 30 de março de 2017</a>, publicamos aqui as
+                        target="_blank" class="hover:underline transition-all">Lei 13.425 de 30 de março de 2017</a>,
+                    publicamos aqui as
                     licenças e certificados
                     concedidos a essa empresa dando ciência de sua habilidade para o fabrico de águas envasadas. Para
                     vista dos interessados e órgãos competentes.

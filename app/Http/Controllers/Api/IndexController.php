98824a368d67f216db84741c32b7651457023a91
Niltond3 <niltond83@gmail.com>
2025-05-09 21:01:06 -0300

    price filter

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -551,18 +551,31 @@ class IndexController extends Controller
 
                 $effectiveDistributorId = $this->getEffectiveDistributorId($idDistribuidor);
 
-				$produtos = Preco::selectRaw("preco.*, produto.id as idProd, produto.nome as nome, produto.descricao as descricao, produto.img as img, categoria.nome as categoria, estoque.id as idEstoque")
+				$produtos = Preco::selectRaw(
+                    "preco.*,
+                    produto.id as idProd,
+                    produto.nome as nome,
+                    produto.descricao as descricao,
+                    produto.img as img,
+                    categoria.nome as categoria,
+                    estoque.id as idEstoque")
 					->leftJoin("produto", 'produto.id', '=', 'preco.idProduto')
-					->leftJoin('categoria', 'categoria.id', '=', 'produto.idCategoria')
-					->leftJoin('estoque', function($join) use ($effectiveDistributorId) {
+                    ->leftJoin('estoque', function($join) use ($effectiveDistributorId) {
                         $join->on('estoque.idProduto', '=', 'produto.id')
                         ->where('estoque.idDistribuidor', '=', $effectiveDistributorId);
                     })
-					->whereRaw("preco.status = ".Preco::ATIVO." AND preco.idDistribuidor = ".$idDistribuidor. " AND estoque.quantidade >= 1 ".
-					" AND ((preco.inicioValidade IS NULL OR preco.inicioValidade <= CURDATE()) AND (preco.fimValidade IS NULL OR preco.fimValidade >= CURDATE())) ".
-					" AND ((preco.inicioHora IS NULL OR preco.inicioHora <= CURTIME()) AND (preco.fimHora IS NULL OR preco.fimHora > CURTIME())) AND preco.idCliente IS NULL")
-					->orderByRaw("categoria.nome ASC, produto.nome, preco.qtdMin ASC")
-					->get();
+					->leftJoin('categoria', 'categoria.id', '=', 'produto.idCategoria')
+					->whereRaw(
+                        "preco.status = ".Preco::ATIVO." AND
+                        preco.idDistribuidor = ".$idDistribuidor. " AND
+                        estoque.quantidade >= 1 "." AND
+                        ((preco.inicioValidade IS NULL OR preco.inicioValidade <= CURDATE()) AND
+                        (preco.fimValidade IS NULL OR preco.fimValidade >= CURDATE())) "." AND
+                        ((preco.inicioHora IS NULL OR preco.inicioHora <= CURTIME()) AND
+                        (preco.fimHora IS NULL OR preco.fimHora > CURTIME())) AND
+                        preco.idCliente IS NULL")
+                        ->orderByRaw("categoria.nome ASC, produto.nome, preco.qtdMin ASC")
+                        ->get();
 
 				if(count($produtos)){
 					//MONTA JSON DE PRODUTOS
